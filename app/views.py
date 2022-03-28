a885bc7a0550bdd35b87529b19b4902164bd8858
kevohal <kevar.halstead@mymona.edu.jm>
2022-03-28 07:43:18 -0500

    Step 4, part 3

--- app/views.py
+++ app/views.py
@@ -10,6 +10,7 @@ from app import app
 from flask import render_template, request, redirect, url_for, flash
 from app import mail
 from flask_mail import Message
+from .forms import ContactForm
 
 ###
 # Routing for your application.
@@ -23,15 +24,29 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Kevar Halstead")
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     """Render the website's contact page."""
-    form = contactForm()
+    form = ContactForm()
+
     if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('contact.html',form=contactForm)
+        name = form.name.data
+        email = form.email.data
+        subject = form.subject.data
+        message = form.message.data
+
+        flash('You have successfully filled out the form', 'success')
+        
+        msg = Message(subject, sender=(name, email), recipients=["to@example.com"])
+        msg.body = message
+        mail.send(msg)
+
+        flash('Your message was sent successfully!')
+        return redirect('/')
+
+    return render_template('contact.html', form=form)
 
 
 ###
