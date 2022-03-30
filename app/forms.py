d78544b21c446bcadc47ac530b8a2af5366a111b
kevohal <kevar.halstead@mymona.edu.jm>
2022-03-30 18:50:37 -0500

    Final commit  without css

--- app/forms.py
+++ app/forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import DataRequired, Email
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 
 class ContactForm(FlaskForm):
-    name = StringField ('Name', validators = [DataRequired()])
-    email = StringField ('Email', validators = [DataRequired(), Email()])
-    subject = StringField ('Subject', validators = [DataRequired()])
-    message = TextAreaField ('Message', validators = [DataRequired()])
+    name = StringField ('Name', validators = [DataRequired('(Required)')])
+    email = StringField ('Email', validators = [DataRequired('(Required)'), Email()])
+    subject = StringField ('Subject', validators = [DataRequired('(Required)')])
+    message = TextAreaField ('Message', validators = [DataRequired('(Required)')])
     
